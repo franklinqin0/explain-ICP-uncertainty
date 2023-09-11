e43dbb8d422139328c5959b4df0ebeaedf94433b
franklinqin0 <franklinqin0@gmail.com>
2023-09-11 08:24:13 +0200

    set nice level

--- background.py
+++ background.py
@@ -4,12 +4,12 @@ import multiprocessing
 from subprocess import Popen
 
 def main(seq, sn, iu):
-    cmd = ["python3", "uncertainty.py", seq, str(sn), str(iu)]
+    cmd = ["nice -n 10", "python3", "uncertainty.py", seq, str(sn), str(iu)]
     p = Popen(cmd)
     p.wait()
 
 if __name__ == "__main__":
     dataset = Dataset()
     with multiprocessing.Pool() as pool:
-        args = [(seq, round(sn, 1), round(iu, 1)) for seq in dataset.sequences for sn in np.arange(0, 2.1, 0.1) for iu in np.arange(1, 3.1, 0.1)]
+        args = [(seq, round(sn, 1), round(iu, 1)) for seq in dataset.sequences for sn in np.arange(0, 0.7, 0.1) for iu in np.arange(1, 3.1, 0.1)]
         pool.starmap(main, args)
